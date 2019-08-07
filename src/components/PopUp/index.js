b2daac3f7799119b532ebdf33d1fe92ac56f7c6f
GugaDavi <gguedes.tech@gmail.com>
2019-08-07 04:50:55 -0300

    Ferramentas complementares

---
+++ src/components/PopUp/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Button, Text, View } from 'react-native';
+import Modal from 'react-native-modal';
+
+export default class ModalTester extends Component {
+  state = {
+    isModalVisible: false,
+  };
+
+  toggleModal = () => {
+    const { isModalVisible } = this.state;
+    this.setState({ isModalVisible });
+  };
+
+  render() {
+    const { isModalVisible } = this.state;
+    return (
+      <View style={{ flex: 1 }}>
+        <Button title="Show modal" onPress={this.toggleModal} />
+        <Modal isVisible={isModalVisible}>
+          <View style={{ flex: 1 }}>
+            <Text>Hello!</Text>
+            <Button title="Hide modal" onPress={this.toggleModal} />
+          </View>
+        </Modal>
+      </View>
+    );
+  }
+}
--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,10 +1,14 @@
 import Reactotron from 'reactotron-react-native';
+import reactotronSaga from 'reactotron-redux-saga';
+import { reactotronRedux } from 'reactotron-redux';
 
-if (__DEV__) {
-  const tron = Reactotron.configure({ hots: '10.0.0.1' })
+if (process.env.NODE_ENV === 'development') {
+  const tron = Reactotron.configure({ host: '10.0.0.142' })
     .useReactNative()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
     .connect();
-  console.tron = tron;
-
   tron.clear();
+
+  console.tron = tron;
 }
